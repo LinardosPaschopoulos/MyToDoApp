6b14d4bb723dd7edd3bbefdf7c9c862fa6e4f8d4
Linardos Paschopoulos <linardos.paschopoulos@atcom.gr>
2025-12-31 12:45:51 +0200

    initial commit

---
+++ MyToDoApp/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  MyToDoApp
+//
+//  Created by Linardos Paschopoulos  on 31/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .foregroundColor(Color(.systemBlue))
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyToDoApp/MyToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyToDoAppApp.swift
+//  MyToDoApp
+//
+//  Created by Linardos Paschopoulos  on 31/12/25.
+//
+
+import SwiftUI
+
+@main
+struct MyToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
